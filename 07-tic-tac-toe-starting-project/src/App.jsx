8578c20d723c1f41c694bc1412da7f93977ef809
rlawogus65@gmail.com <rlawogus65@gmail.com>
2024-04-07 23:45:50 +0900

    24.04.07 React

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,10 +1,18 @@
+import Player from './components/Player.jsx';
+import GameBoard from './components/GameBoard.jsx';
 
 function App() {
-  
-
   return (
-    <p>Coming soon... </p>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="O" />
+        </ol>
+        <GameBoard />
+      </div>
+    </main>
+  );
 }
 
 export default App
---
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+export default function Player({ initialName, symbol }) {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false); // false는 현재 수정 중이 아니라는 의미
+
+    function handleEditClick() {
+        setIsEditing((editing) => !editing); // editing : false -> true
+    }
+
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+    // let btnCaption = 'Edit';
+
+    if (isEditing) {
+        editablePlayerName = <input type="text" required defaultValue={playerName} onChange={handleChange} />;
+        // btnCaption = 'Save';
+    }
+
+    return (
+        <li>
+            <span className="player">
+                {editablePlayerName}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+        </li>
+    );
+}
